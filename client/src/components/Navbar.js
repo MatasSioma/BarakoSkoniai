8d8f7cd92f2b821b5e3e9704ea27f80fe72a3a4e
MatasSioma <matassemenas@gmail.com>
2023-10-28 15:38:48 +0300

    nav style adjustment

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,18 +2,24 @@ import React from "react";
 import logo from "../logo.svg";
 import "./NavbarStyles.css";
 
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
-      <div id="logo"><a href="#" className="logo-link">
+      <div id="logo"><a href="/" className="logo-link">
           <img src={logo} alt="Logo" className="logo" />
           <span className="logotext">BarakoSkoniai</span></a>
       </div>
 
       <div id="navbar">
         <ul className="button-list">
-          <li><a className="nav-button" href="#">Explore recipes</a></li>
+          {/* <li><a className="nav-button" href="#">Explore recipes</a></li>
           <li><a className="nav-button" href="#">Generate with AI</a></li>
+          <li><a className="nav-button signup-button" href="#">Sign Up</a></li>
+          <li><a className="nav-button signin-button" href="#">Sign In</a></li> */}
+          <li><a className="nav-button" href="/users">Users list</a></li>
+          <li><a className="nav-button" href="/new">basic new recipe</a></li>
           <li><a className="nav-button signup-button" href="#">Sign Up</a></li>
           <li><a className="nav-button signin-button" href="#">Sign In</a></li>
         </ul>
