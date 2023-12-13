72bdccbf584bfa5935d9e9f009e42a1d543e94a9
Plexukas <algirdasleonardas@gmail.com>
2023-12-13 20:12:53 +0200

    introduced allRecipes api

--- client/src/components/Explore.js
+++ client/src/components/Explore.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ExploreStyles.css";
 import SmallRecipe from "./SmallRecipe.js";
 import "./SmallRecipeStyles.css";
-
 import searchItem from "../images/outline.svg";
 
+function chunkArray(array, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
+
 function Explore() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch("/api/allRecipes");
+        if (!response.ok) throw new Error("Network response was not ok");
+        const data = await response.json();
+        setRecipes(data);
+      } catch (error) {
+        console.error("Failed to fetch recipes:", error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
+  // Group recipes into sets of three
+  const groupedRecipes = chunkArray(recipes, 3);
+
   return (
     <exp>
       <div className="start-container mx-auto justify-between items-center flex direction-column">
@@ -15,20 +42,27 @@ function Explore() {
           <br />
           Come, cook and create with us!
         </h2>
-        <button id="search"><img src={searchItem} alt="Search" />Search</button>
+        <button id="search">
+          <img src={searchItem} alt="Search" />
+          Search
+        </button>
       </div>
 
-      <div className="recipe-container mx-auto flex justify-between">
-        <div className="small-recipe">
-          <SmallRecipe recipe={{ id: "9" }} loadUserIngredients={false} />
+      {groupedRecipes.map((group, index) => (
+        <div
+          key={index}
+          className="recipe-container mx-auto flex justify-between"
+        >
+          {group.map((recipe) => (
+            <div key={recipe.id} className="small-recipe">
+              <SmallRecipe
+                recipe={{ id: recipe.id }}
+                loadUserIngredients={false}
+              />
+            </div>
+          ))}
         </div>
-        <div className="small-recipe">
-          <SmallRecipe recipe={{ id: "21" }} loadUserIngredients={false} />
-        </div>
-        <div className="small-recipe">
-          <SmallRecipe recipe={{ id: "24" }} loadUserIngredients={false} />
-        </div>
-      </div>
+      ))}
     </exp>
   );
 }
