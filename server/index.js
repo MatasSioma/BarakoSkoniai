e78db1fa6b0f326c5944963db4d5ec2524d94443
Vaardenis Pavardenis <marijastudy@gmail.com>
2023-12-15 13:37:12 +0200

    c

--- server/index.js
+++ server/index.js
@@ -8,4 +8,6 @@ app.use(express.static(path.join(__dirname + "/public")))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
