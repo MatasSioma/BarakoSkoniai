c39f54993c30528245357a4906870dc7ac5d4c1e
Plexukas <algirdasleonardas@gmail.com>
2023-12-21 11:41:56 +0200

    Add mobile menu functionality to Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../images/logo.svg';
 import './NavbarStyles.css';
 import { Logout } from './Logout';
@@ -6,35 +6,77 @@ import { useAuth } from './AuthContext';
 
 function Navbar() {
   const { isAuthenticated } = useAuth();
+  const [isMobile, setIsMobile] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 769 ? setIsMobile(true) : setIsMobile(false);
+    };
+
+    setResponsiveness();
+
+    window.addEventListener('resize', setResponsiveness);
+
+    return () => {
+      window.removeEventListener('resize', setResponsiveness);
+    };
+  }, []);
 
   return (
     <nav>
       <div className='nav-container'>
-      <div id="logo">
-        <a href="/" className="logo-link">
-          <img src={logo} alt="Logo" className="logo" />
-          <span className="logotext">BarakoSkoniai</span>
-        </a>
-      </div>
+        <div id="logo">
+          <a href="/" className="logo-link">
+            <img src={logo} alt="Logo" className="logo" />
+            <span className="logotext">BarakoSkoniai</span>
+          </a>
+        </div>
 
-      <div id="navbar">
-        <ul className="button-list">
-          <li><a className="nav-button" href="/explore">Explore recipes</a></li>
-          <li><a className="nav-button" href="/new">Generate with AI</a></li>
-          {isAuthenticated ? (
-            <>
-            <li><a className="nav-button signup-button" href="/Users">Profile</a></li>
-            <li><a className="nav-button signin-button" href="/Login" onClick={() => Logout()}>Logout</a></li>
-            </>
-            
-          ) : (
-            <>
-              <li><a className="nav-button signup-button" href="/Register">Sign Up</a></li>
-              <li><a className="nav-button signin-button" href="/Login">Sign In</a></li>
-            </>
-          )}
-        </ul>
-      </div>
+        {isMobile ? (
+          <div>
+            <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+              ☰
+            </div>
+            <ul className={`button-list ${showMobileMenu ? 'show' : ''}`}>
+              <li><a href="/explore">Explore recipes</a></li>
+              <li><a href="/new">Generate with AI</a></li>
+              {isAuthenticated ? (
+                <>
+                  <li><a href="/Users">Profile</a></li>
+                  <li><a href="/Login" onClick={() => { Logout(); setShowMobileMenu(false); }}>Logout</a></li>
+                </>
+              ) : (
+                <>
+                  <li><a href="/Register">Sign Up</a></li>
+                  <li><a href="/Login">Sign In</a></li>
+                </>
+              )}
+            </ul>
+          </div>
+        ) : (
+          <div id="navbar">
+            <ul className="button-list">
+              <li><a className="nav-button text-button" href="/explore">Explore recipes</a></li>
+              <li><a className="nav-button text-button" href="/new">Generate with AI</a></li>
+              {isAuthenticated ? (
+                <>
+                  <li><a className="nav-button signup-button" href="/Users">Profile</a></li>
+                  <li><a className="nav-button signin-button" href="/Login" onClick={() => Logout()}>Logout</a></li>
+                </>
+              ) : (
+                <>
+                  <li><a className="nav-button signup-button" href="/Register">Sign Up</a></li>
+                  <li><a className="nav-button signin-button" href="/Login">Sign In</a></li>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
